68cde312bccc4568b4486e43d19e5f6e8733c60e
Ross Landino <mrlandino@gmail.com>
2022-02-02 21:51:47 -0700

    Complete warmup and up to saveReview

--- src/book.js
+++ src/book.js
@@ -1,11 +1,30 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name : name,
+    age : age,
+    pronouns : pronouns,
+  };
+  return character;
+}
+
+function saveReview(reviewInput, reviews) {
+  if (reviews.indexOf(reviewInput) === -1) {
+    reviews.push(reviewInput);
+    return reviews;
+  } else if (reviews.indexOf(reviewInput) > -1) {
+    return reviews;
+  }
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
