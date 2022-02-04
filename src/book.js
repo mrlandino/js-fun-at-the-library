a8c0a1ca7226e0d8f39add5213ab0f83714d12f6
Ross Landino <mrlandino@gmail.com>
2022-02-03 21:12:06 -0700

    Complete test

--- src/book.js
+++ src/book.js
@@ -2,22 +2,66 @@ function createTitle(title) {
   return `The ${title}`;
 }
 
-function buildMainCharacter(name, age, pronouns) {
+function buildMainCharacter(nameInput, ageInput, pronounsInput) {
   var character = {
-    name : name,
-    age : age,
-    pronouns : pronouns,
+    name : nameInput,
+    age : ageInput,
+    pronouns : pronounsInput,
   };
   return character;
 }
 
+// function saveReview(reviewInput, reviews) {
+//   if (reviews.indexOf(reviewInput) === -1) {
+//     reviews.push(reviewInput);
+//     return reviews;
+//   } else if (reviews.indexOf(reviewInput) > -1) {
+//     return reviews;
+//   }
+// }
+
+
 function saveReview(reviewInput, reviews) {
-  if (reviews.indexOf(reviewInput) === -1) {
-    reviews.push(reviewInput);
-    return reviews;
-  } else if (reviews.indexOf(reviewInput) > -1) {
-    return reviews;
+  for (var i = 0; i < reviews.length + 1; i++) {
+    if (reviewInput !== reviews[i]) {
+      reviews.push(reviewInput);
+      return reviews;
+    } else {
+      return 
+    }
   }
+}
+
+function calculatePageCount(bookTitle) {
+  //console.log("$$", bookTitle);
+  //input: bookTitle - string
+  //output: should be 340
+  //check how many letters are in the string bookTitle
+  //taking that number of characters (including spaces) in the string and * by 20
+  //return the result number
+  //console.log(bookTitle.length)
+    return (bookTitle.length * 20);
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  //output object
+  //input
+  var book = {
+    title : bookTitle,
+    mainCharacter : bookCharacter,
+    pageCount : calculatePageCount(bookTitle),
+    genre : bookGenre,
+  }
+  return book;
+}
+
+// function editBook(ghoulBook) {
+//   ghoulBook.pageCount = 255;
+//     //console.log(ghoulBook);
+// }
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
+  return book;
 }
 
 
@@ -25,7 +69,7 @@ module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
+function shelfBook(book, sciFiShelf) {
+  if (sciFiShelf.length < 3) {
+    sciFiShelf.unshift(book);
+    //console.log(book);
+    return sciFiShelf;
+  } else {
+    return sciFiShelf;
+  }
+}
+
+function unshelfBook(book, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (book === sciFiShelf[i].title) {
+      sciFiShelf.splice(i, 1);
+      // return sciFiShelf;
+    }
+  }
+}
+
+function listTitles(fantasyShelf) {
+   //for (var i = 0; i < fantasyShelf.length - 1; i++) {
+   //console.log(fantasyShelf[i].title);
+      var bookList = `${fantasyShelf[0].title}, ${fantasyShelf[1].title}, ${fantasyShelf[2].title}`;
+      return bookList;
+}
+
+function searchShelf(sciFiShelf, bookTitle) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === bookTitle) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
